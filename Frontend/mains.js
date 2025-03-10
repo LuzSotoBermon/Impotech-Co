7b6b50175370baaa0d8b1d2e4329111379fa9bc6
LuzSotoBermon <sotolucerito1945@gmail.com>
2025-03-10 17:27:36 -0500

    Delete Frontend directory

--- Frontend/mains.js
+++
@@ -1,8 +0,0 @@
-// Obtener el usuario de sessionStorage
-const usuario = sessionStorage.getItem("usuario");
-
-if (usuario) {
-    document.getElementById("email-usuario").innerText = usuario;
-} else {
-    window.location.href = "../Inicio de sesion/index.html"; // Redirige si no hay sesión activa
-}
